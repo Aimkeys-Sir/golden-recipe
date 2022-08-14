757b25ba5a0a477b9330c8026ce2abb8d165c109
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-08-14 04:35:56 +0300

    Data edit

--- src/Data.js
+++ src/Data.js
@@ -9,22 +9,22 @@ const recipes = [
         description: "Pilau  is the king of traditional rice cuisine among the Swahili community. Culturally, it is a must cook dishon Friday particularly during lunch. It is enjoyed by the entire family and often served with tomatoes, onions and chillies salad commonly known as (“kachumbari” or salsa) and accompanied with a ripe banana.This tastefully spiced dish is characterized by whole potatoes and meat chunks as key ingredients.",
         picture: "https://www.malindikenya.net/images/uploads/articoli/1163_l.jpg",
         ingredients: [
-            { "raw white rice": [595, "3 1/3 cups"] },
-            { "beef": [500, "1/2kg"] },
-            { "cardamom, whole": [1, "10 seeds"] },
-            { "cinnamon, whole": [3, "5 sticks"] },
-            { "black pepper, whole": [1, "5 seeds"] },
-            { "cloves, whole": [1, "5 seeds"] },
-            { "salt, iodized": [30, "6 tsp"] },
-            { "cumin": [11, "2 tsp"] },
-            { "potatoes unpeeled raw": [840, "4"] },
-            { "red skinned onions, unpeeled raw": [387, "3"] },
-            { "Garlic, whole": [17, "5 cloves"] },
-            { "Ginger": [23, "1 root"] },
-            { "green capsicum": [115, "1/2"] },
-            { "Coriander": [62, "fresh"] },
-            { "water": [1788, "8 1/4 cups"] },
-            { "cooking oil": [200, "1/2 cup"] }
+            { ingName:"raw white rice",amount: [595, "3 1/3 cups"] },
+            { ingName:"beef",amount: [500, "1/2kg"] },
+            { ingName:"cardamom, whole",amount: [1, "10 seeds"] },
+            { ingName:"cinnamon, whole",amount: [3, "5 sticks"] },
+            { ingName:"black pepper, whole",amount: [1, "5 seeds"] },
+            { ingName:"cloves, whole",amount: [1, "5 seeds"] },
+            { ingName:"salt, iodized",amount: [30, "6 tsp"] },
+            { ingName:"cumin",amount: [11, "2 tsp"] },
+            { ingName:"potatoes unpeeled raw",amount: [840, "4"] },
+            { ingName:"red skinned onions, unpeeled raw",amount: [387, "3"] },
+            { ingName:"Garlic, whole",amount: [17, "5 cloves"] },
+            { ingName:"Ginger",amount: [23, "1 root"] },
+            { ingName:"green capsicum",amount: [115, "1/2"] },
+            { ingName:"Coriander",amount: [62, "fresh"] },
+            { ingName:"water",amount: [1788, "8 1/4 cups"] },
+            { ingName:"cooking oil",amount: [200, "1/2 cup"]}
         ],
         instructions: [
             "Cut meat into medium size chunks.",
@@ -53,10 +53,10 @@ const recipes = [
         description: "This dish is common among the Swahili community in Kenya. The cuisine is also meat based and special emphasis is given to finding the right kinds of rice, spices, meat and herbs. It is simply steamed rice eaten with Biryani stew. It is particularly made using the fragrant species of rice. This colourful dish can be served for lunch, dinner or during wedding occasions.",
         picture: "https://i.pinimg.com/564x/d8/3a/06/d83a0696cb9a7d2437f36a9e999b97ac.jpg",
         ingredients: [
-            { "raw white rice": [1000, "5 3/4 cups"] },
-            { "water": [1874, "8 2/3 cups"] },
-            { "salt": [10, "2 tsp"] },
-            { "cooking oil": [85, "7 tsp"] }
+            { ingName:"raw white rice",amount: [1000, "5 3/4 cups"] },
+            { ingName:"water",amount: [1874, "8 2/3 cups"] },
+            { ingName:"salt",amount: [10, "2 tsp"] },
+            { ingName:"cooking oil",amount: [85, "7 tsp"] }
         ],
         instructions: [
             "Take out 2 tablespoons of water and put the rest ofthe water into a cooking pot, add salt and bring to a boil.",
